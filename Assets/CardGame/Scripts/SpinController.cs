4b1279e54d5d7efb6f71d89e4418a821e6da9454
ismail <bergama_itu@hotmail.com>
2023-04-21 20:02:10 +0300

    + Added: Spin controller

---
+++ Assets/CardGame/Scripts/SpinController.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using CardGame.Wheel;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+
+namespace CardGame
+{
+    [RequireComponent(typeof(Button))]
+    public class SpinController : MonoBehaviour
+    {
+        public event Action DidFinishedSpin;
+
+        [Header("Design")] private bool _isRandom = true;
+        private int _spinSliceCount;
+
+        [Header("Development")] [SerializeField]
+        private RewardCounter _rewardCounter;
+
+        [SerializeField] private GameObject _deadPanel;
+
+
+        private WheelAbstract _wheel;
+        private Transform _wheelTransform;
+        private float _sliceAngleInterval;
+        private Button _button;
+
+
+        private void Awake()
+        {
+            _button = GetComponent<Button>();
+            _button.onClick.AddListener(Spin);
+        }
+
+
+        public void ResetSpinning()
+        {
+            if (_wheelTransform) _wheelTransform.DOKill();
+        }
+
+
+        public void ChangeInteractable(bool value) => _button.interactable = value;
+
+
+        public void Init(WheelAbstract wheel, Transform wheelTransform)
+        {
+            _wheel = wheel;
+            _wheelTransform = wheelTransform;
+        }
+
+
+        private void Spin()
+        {
+            ChangeInteractable(false);
+
+            if (_isRandom) _spinSliceCount = Random.Range(15, 35);
+
+            var targetZAngle = _spinSliceCount * 45;
+
+            _wheelTransform.DORotate(new Vector3(0, 0, targetZAngle), 3, RotateMode.FastBeyond360)
+                .OnComplete(() => FinishSpin(_spinSliceCount % 8));
+        }
+
+
+        private void FinishSpin(int index)
+        {
+            if (_wheel.SlicesOfWheelData[index].RewardType == RewardType.Dead) _deadPanel.SetActive(true);
+
+            _rewardCounter.AddReward(_wheel.SlicesOfWheelData[index].RewardType, _wheel.SlicesOfWheelData[index].Count);
+            DidFinishedSpin?.Invoke();
+        }
+    }
+}
